670d501ae27dc67fc937eceb8b994fdbeaf2a550
Valeria Ali <vsalif123@gmail.com>
2023-07-19 15:04:07 -0600

    merge de archivos binarios, cache

---
+++ project_management_for_ISOGES/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project_management_for_ISOGES.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
