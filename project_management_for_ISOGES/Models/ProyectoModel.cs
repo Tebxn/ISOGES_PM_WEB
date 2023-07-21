1d0443eff08ebac6af86fe0f6b0c4503750bf5b7
Sharon-MN <77652147+Sharon-MN@users.noreply.github.com>
2023-07-21 17:08:13 -0600

    Se crea el modelo y controller de Proyecto
    Modelo: crear proyecto, consultar proyecto
    Controller: Consultar proyectos

---
+++ project_management_for_ISOGES/Models/ProyectoModel.cs
@@ -0,0 +1,50 @@
+﻿using project_management_for_ISOGES.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+
+namespace project_management_for_ISOGES.Models
+{
+    public class ProyectoModel
+    {
+        public int CrearProyecto(ProyectoEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlApi"].ToString() + "api/CrearProyecto";
+                JsonContent body = JsonContent.Create(entidad); //Serializar
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public List<ProyectoEnt> ConsultarProyectos()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlApi"].ToString() + "api/ConsultarProyectos";
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<List<ProyectoEnt>>().Result;
+                }
+
+                return new List<ProyectoEnt>();
+            }
+        }
+
+
+
+    }
+}
