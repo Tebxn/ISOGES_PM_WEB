42aff41ff37d7474a28c45dff47906fe83f4fe4f
Saul Hernandez <saulhdez28@gmail.com>
2023-07-22 16:43:34 -0600

    Crear CRUD Requerimientos. Falta Eliminar y subir archivos.

---
+++ project_management_for_ISOGES/Models/RequerimientoModel.cs
@@ -0,0 +1,98 @@
+﻿using project_management_for_ISOGES.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Web;
+
+namespace project_management_for_ISOGES.Models
+{
+    public class RequerimientoModel
+    {
+
+        public int CrearRequerimiento(RequerimientoEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlApi"].ToString() + "api/CrearRequerimiento";
+                JsonContent body = JsonContent.Create(entidad); //Serializar
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public List<RequerimientoEnt> ConsultarRequerimientos()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlApi"].ToString() + "api/ConsultarRequerimientos";
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<List<RequerimientoEnt>>().Result;
+                }
+
+                return new List<RequerimientoEnt>();
+            }
+        }
+
+        public int EliminarRequerimiento(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlApi"].ToString() + "api/EliminarRequerimiento";
+                HttpResponseMessage resp = client.DeleteAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public RequerimientoEnt ConsultaRequerimientoPorId(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlApi"].ToString() + "api/ConsultarRequerimientoPorId?q=" + q;
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<RequerimientoEnt>().Result;
+                }
+
+                return null;
+            }
+        }
+
+        public int EditarRequerimiento(RequerimientoEnt entidad)
+        {
+            using (var requerimiento = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlApi"].ToString() + "api/EditarRequerimiento";
+                JsonContent body = JsonContent.Create(entidad); //Serializar
+                HttpResponseMessage resp = requerimiento.PutAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+    }
+}
