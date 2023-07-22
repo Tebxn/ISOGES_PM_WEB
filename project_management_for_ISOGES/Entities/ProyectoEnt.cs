065bc28af8a75f769589b6396f06d63ec3ae44fb
Sharon-MN <77652147+Sharon-MN@users.noreply.github.com>
2023-07-22 17:42:23 -0600

    Se agrega Utilitarios al modelo

--- project_management_for_ISOGES/Entities/ProyectoEnt.cs
+++ project_management_for_ISOGES/Entities/ProyectoEnt.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using project_management_for_ISOGES.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace project_management_for_ISOGES.Entities
 {
-    public class ProyectoEnt
+    public class ProyectoEnt 
     {
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public int Cliente { get; set; }
         public bool Estado { get; set; }
         public float MontoEstimado { get; set; }
+    }
+
+    public class ProyectoResponse : Response
+    {
+        public ProyectoEnt ObjectSingle { get; set; } = new ProyectoEnt();
+        public List<ProyectoEnt> ObjectList { get; set; } = new List<ProyectoEnt>();
     }
 }
